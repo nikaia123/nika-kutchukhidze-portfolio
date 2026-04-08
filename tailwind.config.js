8e53bd1536730a1fb4028777157931261f62e305
nika <nikanozkuchu@gmail.com>
2026-04-08 00:38:45 -0700

    init: Vite + React + TypeScript + Tailwind + Router setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.hrml ', 'src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
