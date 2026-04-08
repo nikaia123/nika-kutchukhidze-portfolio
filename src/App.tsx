4e05d352fbeaec017f2a54782aaf1a07015b376c
nika <nikanozkuchu@gmail.com>
2026-04-08 00:38:45 -0700

    init: Vite + React + TypeScript + Tailwind + Router setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  <BrowserRouter>
+  </BrowserRouter>
+}
+
+export default App
