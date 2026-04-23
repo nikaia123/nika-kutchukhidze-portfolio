4b3cbc61d69b1c9cf35e464af047a2e93942c05b
nika <nikanozkuchu@gmail.com>
2026-04-22 23:13:20 -0700

    feat: configure React Router with all routes and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,23 @@ import { Contact } from './pages/Contact';
 import { Projects } from './pages/Projects';
 import { Terms } from './pages/Terms';
 import { Privacy } from './pages/Privacy';
+import { NotFound } from './pages/NotFound';
 import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
       <MainLayout>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Projects' element={<Projects />} />
-          <Route path='/Terms' element={<Terms />} />
-          <Route path='/Privacy' element={<Privacy />} />
-          <Route path='/ScrollToTop' element={<ScrollToTop/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-        
       </MainLayout>
     </BrowserRouter>
   );
