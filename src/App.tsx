00b2eb4cde8a81ccdbdfcf596907cce0f5bbd4ef
nika <nikanozkuchu@gmail.com>
2026-05-05 23:52:02 -0700

    feat: initialize React application with routing and Netlify deployment configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout';
